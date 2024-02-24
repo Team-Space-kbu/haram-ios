42583a9d1014e91b02801eac033271a938e6c09a
dlrjswns <leekyunjun@gmail.com>
2024-02-24 20:09:50 +0900

    [FEAT] svg이미지 파싱을 위한 작업

---
+++ Haram/Haram/Network/Config.swift
@@ -0,0 +1,18 @@
+//
+//  Config.swift
+//  Haram
+//
+//  Created by 이건준 on 2023/04/01.
+//
+
+import Foundation
+
+enum NaverMapKeyConstants {
+  static let clientID = "ebw01b3ehd"
+}
+
+enum URLConstants {
+  static let baseURL = "https://test.team-space.org"
+//  static let baseURL = "https://api.team-space.org"
+  static let intranetBaseURL = "https://m.bible.ac.kr"
+}
