19cd6c0affd4b672098b971eba8a9cb6032a5b4b
dlrjswns <leekyunjun@gmail.com>
2024-03-05 16:51:02 +0900

    [FEAT] 오픈라이선스 화면 구현

--- Haram/Haram/Constants/Constants.swift
+++ Haram/Haram/Constants/Constants.swift
@@ -12,4 +12,7 @@ enum Constants {
   /// 한국성서대학교 좌표(제휴업체 초기 좌표)
   static let currentLat = 37.6486885
   static let currentLng = 127.0642073
+  
+  /// 오픈 라이선스 plist 이름
+  static let openLicenseFileName = "Pods-LicenseProject-acknowledgements"
 }
