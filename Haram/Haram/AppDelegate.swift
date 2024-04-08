82df54211993747db0ca1216599577de2b2d8291
dlrjswns <leekyunjun@gmail.com>
2024-04-09 01:04:11 +0900

    [FEAT] Firebase 추가

--- Haram/Haram/AppDelegate.swift
+++ Haram/Haram/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     setupNavigationBarStyle()
-//    FirebaseApp.configure()
+    FirebaseApp.configure()
     
     // Initialize RevisionOfTranslation
     let models = CoreDataManager.shared.getRevisionOfTranslation()
